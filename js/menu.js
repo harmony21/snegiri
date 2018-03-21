943824d4d6a2b640ab11e6d91d7cba311a314418
Inna Botvinnik <21icelady97@gmail.com>
2018-03-21 22:20:05 +1000

    smooth menu

--- js/menu.js
+++ js/menu.js
@@ -6,21 +6,22 @@ $(document).ready(function() {
 
         if(!$(".menu_wrapper").hasClass('active')) {
             $(".menu_wrapper").addClass('active'),
-            $(".menu_wrapper").css('height', '356px'),
+            $('.menu_wrapper').animate({height: 356}, 500),
             $('.page_top').css("height", "1089px");
         }
-        
+
         else {
             $(".menu_wrapper").removeClass('active');
-            $(".menu_wrapper").css('height', '90px'),
+            $('.menu_wrapper').animate({height: 90,}, 500),
             $('.page_top').css("height", "823px");
         }
     });
 
     //подменю в мобильной версии
     $(".top_menu__item").on('click', function (ev) {
-        ev.preventDefault;
+    ev.preventDefault;
 
+    if ($(window).width() < 564) {
         var element = $(ev.target),
         content = element.closest(".inside_menu"),
         verItem = element.closest('.top_menu__item'),
@@ -29,58 +30,25 @@ $(document).ready(function() {
         verContents = verItems.find('.inside_menu__wrap'),
         maxHeight = verItem.find('.inside_menu').outerHeight(true);
         
-
         if(!verItem.hasClass('active')) {
             verItems.removeClass('active');
             verItem.addClass('active');
 
             verContents.css ('height', 0),
             verContent.css('height', maxHeight); 
-            
-            $('.menu_wrapper').css('height', '532px'),
+         
+            $('.menu_wrapper').animate({height: 532}, 500),
             $('.page_top').css("height", "1265px");
 
         } else {
             verItem.removeClass('active');
-
             verContent.css ('height', 0),
-            $('.menu_wrapper').css('height', '356px');
+            $('.menu_wrapper').animate({height: 356}, 500),
             $('.page_top').css('height', '1089px');     
-        }    
-   
-    });
+        } 
+    } 
+});
 
-
-
-    // element.hover(
-    //     function() {
-    //         setTimeout(function() {
-    //             content.show();
-    //         },
-    //         10000);   
-    //     },
-    //     function() {
-    //         clearTimeout(function() {
-    //             content.hide();
-    //         },
-    //         10000);
-    //     }
-    // )
-
-
-    // var timeout;
-    // element.hover (
-    //     function() {
-    //         timeout = setTimeout (function() {
-    //             content.show();
-    //         }, 5000);
-    //     },
-    //     function() {
-    //         clearTimeout(timeout);
-    //         content.hide();
-    //     }
-    // );
-  
     $(window).resize(function() {
         if ($(window).width() > 563 && $(window).width() < 870) {
             $('.page_top').css("height", "853px");
@@ -93,6 +61,7 @@ $(document).ready(function() {
         if ($(window).width() <= 563) {
             $('.page_top').css("height", "823px");
         }
-    }); 
+    });
 });
+
 
