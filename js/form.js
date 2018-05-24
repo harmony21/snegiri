ce3494a23ed15754d17a3ade3c42063be7bb8540
Inna <21icelady97@gmail.com>
2018-05-24 16:13:16 +1000

    pre-final version

--- js/form.js
+++ js/form.js
@@ -29,6 +29,25 @@ $(document).ready(function() {
         $(".wrapper").fadeOut(500);
     })
 
+
+    $('.reviews__publish').on('click', function (ev) {
+        ev.preventDefault;
+        $(".wrapper").fadeIn(400);
+        $(".form-review").fadeIn(400);   
+    });
+     $('.wrapper').on('click', function (ev) {
+        $(".form-review").fadeOut(400);   
+        $(".wrapper").fadeOut(500);
+    })
+    $('.form-review__close-btn').on('click', function (ev) {
+        ev.preventDefault;
+        $(".form-review").fadeOut(400);   
+        $(".wrapper").fadeOut(400);
+    });
+
+
+    
+
 });
 
 
--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
 
-    if ($(window).width() < 564) {
+    if ($(window).width() < 591) {
         $('.top_menu').css("top", "211px");
+        $(".mobile").removeAttr('href');   
     }
 
     //меню в мобильной версии
     $('.mobile-menu').on('click', function (ev) {
         ev.preventDefault;
 
-        if ($(window).width() < 564) {
+        if ($(window).width() < 591) {
 
             if (!$(".menu_wrapper").hasClass('active')) {
-                $('.menu_wrapper').animate({height: 356}, 400),
-                $('.page_top').animate({height: 831,}, 400);
+                $('.menu_wrapper').animate({height: 466}, 400);
                 setTimeout(function() {
                     $(".menu_wrapper").addClass('active');
                 }, 300);   
             }
             else {
                 $(".menu_wrapper").removeClass('active');
-                $('.menu_wrapper').animate({height: 90,}, 400),
-                $('.page_top').animate({height: 565,}, 400),
+                $('.menu_wrapper').animate({height: 90,}, 400);
                 $('.top_menu__item').removeClass('active');
                 $('.top_menu__item').find('.inside_menu__wrap').css("height", "0");
             }  
         }    
     });
 
+
     //подменю в мобильной версии
     $(".inside").on('click', function (ev) {
         ev.preventDefault;
 
-        if ($(window).width() < 564) {
+        if ($(window).width() < 591) {
             var element = $(ev.target),
             verItem = element.closest('.top_menu__item'),
             verItems = verItem.siblings(),
@@ -46,32 +46,30 @@ $(document).ready(function() {
                     setTimeout(function() {
                         verItem.addClass('active');
                     }, 300);  
-                    $('.menu_wrapper').animate({height: 532}, 400);
-                    $('.page_top').css("height", "1007px");
+                    $('.menu_wrapper').animate({height: 616}, 400);
                 } 
                 else {
                     verItem.removeClass('active'); 
                     verContent.animate({height: 0}, 400);
-                    $('.menu_wrapper').animate({height: 356}, 400);
-                    $('.page_top').css('height', '831px');     
+                    $('.menu_wrapper').animate({height: 466}, 400);     
                 }     
             } 
         });
     });
 
 $(window).resize(function() {
-        if ($(window).width() > 563 && $(window).width() < 870) {
-            $('.page_top').css("height", "853px");
+        if ($(window).width() > 590 && $(window).width() < 870) {
+            $(".mobile").attr('href', '..//index.html');
             $('.menu_wrapper').css('height', '90px');
             $(".menu_wrapper").removeClass('active');
         }
         if ($(window).width() >= 890) { 
-            $('.page_top').css("height", "913px");
+            $(".mobile").attr('href', '..//index.html');
             $('.menu_wrapper').css('height', '90px');
             $(".top_menu__item").removeClass('active');
         }
-        if ($(window).width() <= 563) {
-            $('.page_top').css("height", "565px");
+        if ($(window).width() <= 590) {
+            $(".mobile").removeAttr('href');
             $(".menu_wrapper").removeClass('active');
             $(".top_menu__item").removeClass('active');
             $('.inside_menu__wrap').css ('height', 0);
